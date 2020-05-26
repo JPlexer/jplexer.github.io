4b7fc8f278e384cdc927a437c15fc445585a42fb
JPlexer <jplexer.at.wcg@gmail.com>
2020-05-26 18:38:37 +0200

    Add files via upload

--- bday/script.js
+++ bday/script.js
@@ -2,9 +2,12 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const name = urlParams.get('name')
+const lang = urlParams.get('lang')
 function appendHtml(targetC, htmldata) {
     var theDiv = document.getElementById(targetC);
     theDiv.innerHTML = htmldata;
 }
-
+if (lang === "de") {
+	appendHtml('mess', "Alles Gute zum Geburtstag");
+}
 appendHtml('name', name + "!");
